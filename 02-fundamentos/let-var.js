8f407cc91e83e5ead5f81a04f84f3edb5a088c2c
Jader Neira <jader.neira@gmail.com>
2018-12-01 21:47:33 -0500

    let-var.js - pruebas con var

--- 02-fundamentos/let-var.js
+++ 02-fundamentos/let-var.js
@@ -1,3 +1,11 @@
 var nombre = 'Wolverine';
 
+if (true) {
+    var nombre = 'Magneto'
+}
+
+var nombre = 'Wolverine1';
+var nombre = 'Wolverine2';
+var nombre = 'Wolverine3';
+var nombre = 'Wolverine4';
 console.log(nombre);
